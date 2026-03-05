949bc03ebbc9307d062fcbc4fa64f8c738bb1303
FileMagics <india.trendkart@gmail.com>
2026-03-05 17:16:38 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,29 +9,3 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    
-    let targetUrl = "https://www.earthinsider.in";
-    
-    if (event.notification.data && event.notification.data.link) {
-        targetUrl = event.notification.data.link;
-    } else if (event.notification?.fcmOptions?.link) {
-        targetUrl = event.notification.fcmOptions.link;
-    }
-
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
-            for (let i = 0; i < clientList.length; i++) {
-                let client = clientList[i];
-                if (client.url.includes(targetUrl) && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow(targetUrl);
-            }
-        })
-    );
-});
