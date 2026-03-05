5c19cb4f479f6b09bea87611fb0f1c6cb8060958
FileMagics <india.trendkart@gmail.com>
2026-03-05 18:01:47 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,32 +9,3 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-// The Bulletproof Click Handler
-self.addEventListener('notificationclick', function(event) {
-    // 1. Notification ko turant close karo
-    event.notification.close();
-    
-    // 2. N8n ne jo secret "url" data bheja hai, usko nikalo
-    let urlToOpen = "https://www.earthinsider.in"; // Default link
-    if (event.notification.data && event.notification.data.url) {
-        urlToOpen = event.notification.data.url;
-    }
-
-    // 3. Browser ko force karo ki naya tab open kare
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(windowClients) {
-            // Agar article pehle se kisi tab mein khula hai, toh wahan le jao
-            for (let i = 0; i < windowClients.length; i++) {
-                let client = windowClients[i];
-                if (client.url === urlToOpen && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            // Warna 100% naya tab open karo
-            if (clients.openWindow) {
-                return clients.openWindow(urlToOpen);
-            }
-        })
-    );
-});
