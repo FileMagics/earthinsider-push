642c449606425a3c4440b9e8780c887bdb79341c
FileMagics <india.trendkart@gmail.com>
2026-03-06 14:11:40 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,3 +9,19 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+// 1000000% Working Click Handler (Cross-Domain Supported)
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close(); // Click karte hi notification band karo
+
+    // N8n se aaya hua data wala link nikalo
+    let urlToOpen = "https://www.earthinsider.in";
+    if (event.notification.data && event.notification.data.click_url) {
+        urlToOpen = event.notification.data.click_url;
+    }
+
+    // Browser ko direct command - Naya window/tab kholo!
+    event.waitUntil(
+        clients.openWindow(urlToOpen)
+    );
+});
